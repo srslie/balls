b68128e7386092af0bf6bad8b52b084db58ffd49
srslie <aliceruppert@gmail.com>
2021-02-24 01:46:17 -0700

    Make ball class

--- balls.js
+++ balls.js
@@ -10,14 +10,20 @@ const getRandomColor = () => {
   return `rgb(${getRandomNum()}, ${getRandomNum()}, ${getRandomNum()})`
 }
 
-ctx.beginPath()
-let startX = 100
-let startY = 100
-let radius = 50
-ctx.arc(startX, startY, radius, 0, (Math.PI * 2))
-ctx.fillStyle = getRandomColor()
-ctx.fill()
+class Ball {
+  constructor(x, y, r) {
+    this.startX = x;
+    this.startY = y;
+    this.radius = r;
+    this.color = getRandomColor()
+  }
 
+  drawBall() {
+    ctx.beginPath()
+    ctx.arc(this.startX, this.startY, this.radius, 0, (Math.PI * 2))
+    ctx.fillStyle = this.color
+    ctx.fill()
+  }
+}
 
-
-
+new Ball(100, 100, 50).drawBall()
