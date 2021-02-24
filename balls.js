f77f69351fc187143d4f6f5a109ddf4f5fb7bbd4
srslie <aliceruppert@gmail.com>
2021-02-24 01:26:23 -0700

    Make random color helper, fillStyle

--- balls.js
+++ balls.js
@@ -4,10 +4,20 @@ const ctx = canvas.getContext('2d')
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+const getRandomColor = () => {
+  const getRandomNum = () => Math.round(Math.random() * 255)
+  console.log(`rgb(${getRandomNum()}, ${getRandomNum()}, ${getRandomNum()})`)
+  return `rgb(${getRandomNum()}, ${getRandomNum()}, ${getRandomNum()})`
+}
+
 ctx.beginPath()
 let startX = 100
 let startY = 100
 let radius = 50
 ctx.arc(startX, startY, radius, 0, (Math.PI * 2))
+ctx.fillStyle = getRandomColor()
 ctx.fill()
+
+
+
 
