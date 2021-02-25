3296e24ca0a09908ea17529be130d35d5726077a
srslie <aliceruppert@gmail.com>
2021-02-25 00:58:31 -0700

    Add back the class and array populated with init

--- balls.js
+++ balls.js
@@ -1,6 +1,7 @@
-const canvas = document.getElementById('canvas')
-const ctx = canvas.getContext('2d')
-let t = performance.now()
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+let t = performance.now();
+let balls = [];
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -12,69 +13,73 @@ window.addEventListener('resize', () => {
 })
 
 const mouse = {
-  x: 100,
-  y: 100
+  x: undefined,
+  y: undefined
 }
 
 canvas.addEventListener('click', event => {
   mouse.x = event.x
   mouse.y = event.y
-  drawBall()
 })
 
 canvas.addEventListener('mousemove', event => {
   mouse.x = event.x
   mouse.y = event.y
-  drawBall()
 })
 
-const getRandomColor = () => {
-  const getRandomNum = () => Math.round(Math.random() * 255)
-  return `rgb(${getRandomNum()}, ${getRandomNum()}, ${getRandomNum()})`
-}
-// class Ball {
-//   constructor(x, y, r) {
-//     this.startX = x;
-//     this.startY = y;
-//     this.radius = r;
-//     this.color = getRandomColor()
-//   }
+const getRandomNum = () => Math.round(Math.random() * 255)
 
-//   move() {
-//     // ctx.clearRect(0,  0 ,  window.innerWidth, window.innerHeight)
-//     ctx.save()
-//     ctx.translate(150, 150)
-//     ctx.restore()
-//     window.requestAnimationFrame(this.move)
-//   }
-
-//   draw() {
-//     ctx.beginPath()
-//     ctx.arc(this.startX, this.startY, this.radius, 0, (Math.PI * 2))
-//     ctx.fillStyle = this.color
-//     ctx.fill()
-//   }
-
-// }
-
-  // new Ball(100, 100, 50).draw()
-  // new Ball(500, 500, 150).draw()
-
-
-const drawBall = () => {
-    // ctx.clearRect(0, 0, window.innerWidth, window.innerHeight)
-    // const tnow = performance.now()
-    // const timeDiffernce = tnow - t
-    // t = tnow
-    // const movement = timeDiffernce + 10
-    ctx.beginPath()
-    ctx.arc(mouse.x, mouse.y , 30, 0, (Math.PI * 2))
-    ctx.fillStyle = getRandomColor()
-    ctx.fill()
-    // ctx.save()
-    // ctx.rotate( (2 * Math.PI) * timeDifference * .02)
-    // ctx.translate(movement, 0)
-    // ctx.restore()
+const getRandomColor = () => `rgb(${getRandomNum()}, ${getRandomNum()}, ${getRandomNum()})`
+class Ball {
+  constructor() {
+    // this.x = mouse.x;
+    // this.y = mouse.y;
+    this.x = getRandomNum();
+    this.y = getRandomNum();
+    this.radius = getRandomNum();
+    this.color = getRandomColor();
+    this.verticalSpeed = Math.random() * 3 - 1.5;
+    this.horizontalSpeed = Math.random() * 3 - 1.5;
   }
 
-  drawBall();
+  update() {
+    this.x += this.verticalSpeed;
+    this.y += this.horizontalSpeed;
+    //makes 2D vector
+  }
+
+  // move() {
+  //   ctx.save()
+  //   ctx.translate(150, 150)
+  //   ctx.restore()
+  //   window.requestAnimationFrame(this.move)
+  // }
+
+  draw() {
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.radius, 0, (Math.PI * 2))
+    ctx.fillStyle = this.color
+    ctx.fill()
+  }
+}
+
+const init = () => {
+  for (i = 0; i < 10; i++) {
+    balls.push(new Ball())
+  }
+}
+
+const drawBalls = () => {
+  
+}
+
+init()
+console.log(balls)
+
+  const animate = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    
+    requestAnimationFrame(animate)
+  }
+
+  animate()
